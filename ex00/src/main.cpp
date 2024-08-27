e569bcfe0be334ba4f4a87d25dda22a72242efda
JorFik <138692812+JorFik@users.noreply.github.com>
2024-08-27 18:41:09 +0200

    Feature (more tests)

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:41:51 by JFikents          #+#    #+#             */
-/*   Updated: 2024/08/27 18:33:46 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/08/27 18:37:33 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,25 @@ int	main(void)
 	const Animal* dog = new Dog();
 	const WrongAnimal* wrong_meta = new WrongAnimal();
 	const WrongAnimal* wrong_cat = new WrongCat();
+	WrongCat wrong_cat2;
 
 	std::cout << std::endl;
 	std::cout << "Animal: ";
 	meta->makeSound();
 	std::cout << "Cat: ";
 	cat->makeSound();
+	std::cout << "Cat Animal::makesound: ";
+	cat->Animal::makeSound();
 	std::cout << "Dog: ";
 	dog->makeSound();
+	std::cout << "Dog Animal::makesound: ";
+	dog->Animal::makeSound();
 	std::cout << "WrongAnimal: ";
 	wrong_meta->makeSound();
 	std::cout << "WrongCat: ";
 	wrong_cat->makeSound();
+	std::cout << "WrongCat2: ";
+	wrong_cat2.makeSound();
 	std::cout << std::endl;
 
 	delete meta;
